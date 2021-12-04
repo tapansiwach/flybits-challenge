568d75015da259c6e4d5cd096194050c7f40d17b
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-04 15:34:11 -0500

    layout User component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import './User.scss'
 
 function User() {
+  const imagesPath = process.env.PUBLIC_URL + "assets/images/";
   return (
     <div className="user">
-      <h1>User</h1>
+      <label htmlFor="latitude">Latitude</label>
+      <input type="text" name="latitude" id="latitude" />
+      <label htmlFor="longitude">Longitude</label>
+      <input type="text" name="longitude" id="longitude" />
+      <div className="offer">
+        <img src={`${imagesPath}money.jpg`} alt="" width="400px" />
+        <p>Offer Caption</p>
+      </div>
+      <div className="offer">
+        <img src={`${imagesPath}money.jpg`} alt="" width="400px" />
+        <p>Offer Caption</p>
+      </div>
     </div>
   )
 }
