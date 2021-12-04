52ade1767f5c16c237625bb056e75ab95189b9b5
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-04 15:33:53 -0500

    layout Marketer component

--- src/pages/Marketer.jsx
+++ src/pages/Marketer.jsx
@@ -4,7 +4,19 @@ import './Marketer.scss'
 function Marketer() {
   return (
     <div className="marketer">
-      <h1>Marketer</h1>
+      <div className="marketer__sidenav">
+        Side Nav
+        <ul>
+          <li>Branch Name 1</li>
+          <li>Branch Name 2</li>
+          <li>Branch Name 3</li>
+        </ul>
+      </div>
+      <div className="marketer__mainContent">
+        <div className="map">
+          Map
+        </div>
+      </div>
     </div>
   )
 }
