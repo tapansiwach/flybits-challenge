f0377e57f09e6d9acb7d351265792d387e7e657c
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-05 13:02:55 -0500

    provide different Map comopnents to user and marketer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -47,8 +47,24 @@ function MarketerGoogleMap() {
   )
 }
 
+function UserGoogleMap() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+      onClick={(e) => {
+        console.log(`lat`, e.latLng.lat());
+        console.log(`lng`, e.latLng.lng());
+      }}
+    >
+    </GoogleMap>
+  )
+}
+
 const MarketerMap = withScriptjs(withGoogleMap(MarketerGoogleMap));
+const UserMap = withScriptjs(withGoogleMap(UserGoogleMap));
 
 export {
   MarketerMap,
+  UserMap,
 }
