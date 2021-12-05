738f3abac94700eea9f731a2a2bfdf4bbf8024b1
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-05 01:55:05 -0500

    display an InfoWindow when a Marker is clicked

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   GoogleMap,
   withScriptjs,
   withGoogleMap,
   Marker,
+  InfoWindow,
 } from 'react-google-maps';
 import * as branchData from "../data/branches.json";
 
 function GMap() {
+  const [selectedBranch, setSelectedBranch] = useState(null);
   return (
     <GoogleMap
       defaultZoom={10}
@@ -17,8 +19,18 @@ function GMap() {
         <Marker
           key={branch.branchAddress}
           position={{ lat: branch.lat, lng: branch.lng }}
+          onClick={() => setSelectedBranch(branch)}
         />
       ))}
+      {selectedBranch && (
+        <InfoWindow
+          zIndex={1}
+          position={{ lat: selectedBranch.lat, lng: selectedBranch.lng }}
+          onCloseClick={() => setSelectedBranch(null)}
+        >
+          <div>Branch Info</div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   )
 }
