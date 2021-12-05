4b8eccccd061d96b30000357105fb73d1cd8ccb6
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-04 21:34:20 -0500

    place the map within the /marketer route

--- src/pages/Marketer.jsx
+++ src/pages/Marketer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Marketer.scss'
+import Map from './Map';
 
 function Marketer() {
   return (
@@ -14,7 +15,12 @@ function Marketer() {
       </div>
       <div className="marketer__mainContent">
         <div className="map">
-          Map
+          <Map
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
+            loadingElement={<div style={{ height: "100%" }} />}
+            containerElement={<div style={{ height: "100%" }} />}
+            mapElement={<div style={{ height: "100%" }} />}
+          />
         </div>
       </div>
     </div>
