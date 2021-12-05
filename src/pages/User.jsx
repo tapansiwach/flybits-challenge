1bd49804337ae5c5ad1581ebb008600dedb9978a
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-05 11:18:24 -0500

    scaffold for calculating a user's proximity to a branch with an offer

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,6 +5,8 @@ import Map from '../components/Map';
 
 function User() {
   const [offers, setOffers] = useState([]);
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
 
   const getAllOffers = () => {
     const allOffers = branchData.branches
@@ -17,12 +19,26 @@ function User() {
     setOffers(allOffers);
   }, []);
 
+  useEffect(() => {
+    console.log(`lat`, lat);
+    console.log(`lng`, lng);
+    if (lat && lng) {
+      // call a helper function to check proximity from a branch with promotion
+    }
+  }, [lat, lng])
+
   return (
     <div className="user">
       <label htmlFor="latitude">Latitude</label>
-      <input type="text" name="latitude" id="latitude" />
+      <input type="text" name="latitude" id="latitude"
+        value={lat}
+        onChange={e => setLat(e.target.value)}
+      />
       <label htmlFor="longitude">Longitude</label>
-      <input type="text" name="longitude" id="longitude" />
+      <input type="text" name="longitude" id="longitude"
+        value={lng}
+        onChange={e => setLng(e.target.value)}
+      />
       <div className="map">
         <Map
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
