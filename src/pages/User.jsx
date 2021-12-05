9dacc129a9fe39ff323a7515493fdd82f65efa8e
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-05 10:43:37 -0500

    useState to get a dynamic list of offers for users

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './User.scss'
 import * as branchData from "../data/branches.json";
 
 function User() {
-  const imagesPath = process.env.PUBLIC_URL + "assets/images/";
+  const [offers, setOffers] = useState([]);
+
+  const getAllOffers = () => {
+    const allOffers = branchData.branches
+      .flatMap(branch => branch.offers);
+    return allOffers;
+  }
+
+  useEffect(() => {
+    const allOffers = getAllOffers();
+    setOffers(allOffers);
+  }, []);
+
   return (
     <div className="user">
       <label htmlFor="latitude">Latitude</label>
       <input type="text" name="latitude" id="latitude" />
       <label htmlFor="longitude">Longitude</label>
       <input type="text" name="longitude" id="longitude" />
-      {branchData.branches.map((branch) => (
+      {offers?.map(offer => (
         <div className="offer">
-          <img src={branch.offers[0].imgSrc} alt="promotional offer" width="400px" />
-          <p>{branch.offers[0].caption}</p>
+          <img src={offer.imgSrc} alt="promotional offer" width="400px" />
+          <p>{offer.caption}</p>
         </div>
       ))}
     </div>
