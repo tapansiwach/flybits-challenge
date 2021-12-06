85cdf604469ff438b35aec1f19909fb46e478341
Tapan Siwach <siwach.tapan@gmail.com>
2021-12-06 04:37:29 -0500

    use db data for /user

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './User.scss'
-import * as branchData from "../data/branches.json";
+// import * as branchData from "../data/branches.json";
+import { getOffers } from '../firebase/db';
 import { UserMap } from '../components/Map';
 import proximate from '../helpers/proximate';
 
 function User() {
   const [offers, setOffers] = useState([]);
-  const [userLat, setUserLat] = useState(null);
-  const [userLng, setUserLng] = useState(null);
-
-  const getAllOffers = () => {
-    const allOffers = branchData.branches
-      .flatMap(branch => branch.offers);
-    return allOffers;
-  }
+  const [userLat, setUserLat] = useState("");
+  const [userLng, setUserLng] = useState("");
 
   useEffect(() => {
-    const allOffers = getAllOffers();
-    setOffers(allOffers);
+    const loadData = async () => {
+      const allOffers = await getOffers()
+      setOffers(allOffers);
+    }
+    loadData();
   }, []);
 
   useEffect(() => {
     if (userLat && userLng) {
       const relevantOffers = [];
-      for (const branch of branchData.branches) {
+      for (const offer of offers) {
         const nearby = proximate(
-          { lat: branch.lat, lng: branch.lng },
+          { lat: offer.lat, lng: offer.lng },
           { lat: userLat, lng: userLng },
-          branch.circle.radius
+          offer.radius
         );
         if (nearby) {
-          relevantOffers.push(...branch.offers);
+          relevantOffers.push(offer);
         }
       }
       setOffers(relevantOffers);
@@ -44,16 +42,22 @@ function User() {
 
   return (
     <div className="user">
-      <label htmlFor="latitude">Latitude</label>
-      <input type="text" name="latitude" id="latitude"
-        value={userLat}
-        onChange={e => setUserLat(e.target.value)}
-      />
-      <label htmlFor="longitude">Longitude</label>
-      <input type="text" name="longitude" id="longitude"
-        value={userLng}
-        onChange={e => setUserLng(e.target.value)}
-      />
+      <form method="get" class="promotion-form">
+        <div className="form-row">
+          <label htmlFor="latitude">Latitude</label>
+          <input type="text" name="latitude" id="latitude"
+            value={userLat}
+            onChange={e => setUserLat(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="longitude">Longitude</label>
+          <input type="text" name="longitude" id="longitude"
+            value={userLng}
+            onChange={e => setUserLng(e.target.value)}
+          />
+        </div>
+      </form>
       <div className="map">
         <UserMap
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API}`}
@@ -63,9 +67,9 @@ function User() {
           setUserLocation={setUserLocation}
         />
       </div>
-      {offers?.map(offer => (
-        <div className="offer">
-          <img src={offer.imgSrc} alt="promotional offer" width="400px" />
+      {offers?.map((offer, index) => (
+        <div key={index} className="offer">
+          <img src={offer.imgSrc} alt="promotional offer" width="370px" />
           <p>{offer.caption}</p>
         </div>
       ))}
